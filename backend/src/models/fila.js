0984d072513b5c31480c4fd28964e2f6c42b11c7
leovdn <leo_vdn@hotmail.com>
2020-11-05 18:02:10 -0300

    criada organização de pastas para arquivos do banco de dados

---
+++ backend/src/models/fila.js
@@ -0,0 +1,50 @@
+class Fila {
+    constructor(dao) {
+        this.dao = dao
+    }
+
+    createTable() {
+        const sql =
+        `CREATE TABLE IF NOT EXISTS fila (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name Text,
+            game Text)`
+        return this.dao.run(sql)
+    }
+
+    insert(name, game) {
+        return this.dao.run(
+            `INSERT INTO fila (name, game)
+                VALUES (?, ?)`,
+                [name, game]
+        )
+    }
+
+    update(fila) {
+        const { id, name, game } = fila
+        return this.dao.run(
+            `UPDATE fila SET name = ? WHERE id = ?`,
+            [name, id]
+        )
+    }
+
+    delete(id) {
+        return this.dao.run(
+            `DELETE FROM fila WHERE id = ?`,
+            [id]
+        )
+    }
+
+    getById(id) {
+        return this.dao.get(
+            `SELECT * FROM fila WHERE id = ?`,
+            [id]
+        )
+    }
+
+    getAll() {
+        return this.dao.all(`SELECT * FROM fila`)
+    }
+}
+
+module.exports = Fila;
